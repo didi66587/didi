9837e8bb82c88a9661b74d8ace241cc2336c094a
didi66587 <didi999999996@hotmail.com>
2025-08-27 17:24:06 +0300

    Add files via upload

---
+++ didi.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("DİDİLERE KATILIN DİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİ");
+        Console.ReadLine();
+        while (true)
+        {
+            Console.WriteLine("DİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ DİDİDİDİ DİDİ DİDİ DİDİ DİDİ Dİdi");
+        }
+
+    }  
+}       
